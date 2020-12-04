3d355a24211847d67ff0250c91140e6d6faa94cc
KrazyAbyss <75076879+KrazyAbyss@users.noreply.github.com>
2020-12-04 13:33:40 +0100

    Update holaMundo.java

--- src/holaMundo.java
+++ src/holaMundo.java
@@ -1,5 +1,11 @@
 
+
 public class holaMundo {
+	
+public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
+
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
